233cac3fe6c05c718931706dc785f0f176c02f35
Keizo Kita <keizokita1@gmail.com>
2022-04-13 03:43:29 -0300

    confimar em 'main'

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 onclick(() => {
         window.location.href = "resultado.html";
     });
-
-
 newFunction();
 
+var url_resultado = 'resultado.html';
+document.querySelector('.btn_submit').href = url_resultado;
+
 <form method="GET" action="resultado.html">
-     
-     
-</form>
+    <>
+    <input type="text" name="Nome"></input>
+    <input type="text" name="e-mail"></input>
+    <input type="password" name="senha"></input>
+    <input type="number" name="idade"></input>
+    <input type="radio" name="genero"></input>
+    <input type="checkbox"></input>
+    <select name="tipo-sanguineo"></select>
+    <textarea name="sobre-vc"></textarea>
+    </>
+
+</form>;
+
